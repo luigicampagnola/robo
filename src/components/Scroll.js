b3a840747516289bd16e5cf3acb78be490979c0b
luigicampagnola <58444283+luigicampagnola@users.noreply.github.com>
2021-06-13 15:17:40 +0200

    push

---
+++ src/components/Scroll.js
@@ -0,0 +1,17 @@
+import "./Scroll.css";
+
+const Scroll = (props) => {
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        border: "1px solid black",
+        height: "1000px",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Scroll;
---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import { robots } from "../robots";
+import { Component } from "react";
+import Scroll from "../components/Scroll";
+import "./App.css";
+import ErrorBoundry from "../components/ErrorBoundry";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  //loading users
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLocaleLowerCase()
+        .includes(this.state.searchfield.toLocaleLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1 className="f2">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
